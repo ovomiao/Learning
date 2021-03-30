6588e96ae8350b1dbca73e53e0aa95e0b2a4505b
ovomiao <crushoy@gmail.com>
2021-03-30 16:57:25 +0800

    添加了 Kotlin

---
+++ CS/Android/app/src/main/java/moe/maonaing/android/MainActivity.java
@@ -0,0 +1,15 @@
+package moe.maonaing.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
--- CS/Android/kotlinandroid/src/main/java/moe/maonaing/kotlinandroid/MainActivity.kt
+++ CS/Android/kotlinandroid/src/main/java/moe/maonaing/kotlinandroid/MainActivity.kt
@@ -3,9 +3,15 @@ package moe.maonaing.kotlinandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Kotlin-Android
+ *
+ */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
