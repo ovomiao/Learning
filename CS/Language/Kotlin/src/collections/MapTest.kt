eaa004c18f06e96c4d7b0a5963dc4324e6b2510f
ovomiao <crushoy@gmail.com>
2021-03-31 18:49:20 +0800

    修改了 add

--- CS/Language/Kotlin/src/collections/MapTest.kt
+++ CS/Language/Kotlin/src/collections/MapTest.kt
@@ -5,12 +5,18 @@ fun main() {
         "zhangsan" to "张三"
     )
 
-    println(map["zhangsan"])
+    // 获取
+    println(map["zhangsan"]) // 推荐
     map += "lisi" to "李四" // DSL 式
-    map["wangwu"] = "王五"
+    map["wangwu"] = "王五" // 推荐
 
+    // 遍历得到 Entry，不是最简方式
     for (entry in map) {
         println("${entry.key} = ${entry.value}")
+    }
+
+    for ((key, value) in map) {
+        println("$key = $value")
     }
 
 }
